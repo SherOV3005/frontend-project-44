ddfd3ba1c1b800d0b8f8e259ec10fdc207378115
olga sherstnova <sherstnova.olga@mail.ru>
2023-03-17 23:25:17 +0500

    падают автоматические тесты

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const userGreeting = () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name?: ');
-  console.log(`Hi ${userName}!`);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 export default userGreeting;
